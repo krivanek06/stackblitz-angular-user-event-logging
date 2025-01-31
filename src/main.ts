998f78c06831af54dce46ae595eef5063c64df20
krivanek06 <krivanek1234@gmail.com>
2025-01-31 09:30:47 +0100

    using prettier

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+    <h1>
+      Hello from
+      {{ name }}!
+    </h1>
+    <a target="_blank" href="https://angular.dev/overview"> Learn more about Angular </a>
   `,
 })
 export class App {
